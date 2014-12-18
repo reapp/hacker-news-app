cfe3251631630d1ccfb60db63c9a0b86b3e782c9
natew <natewienert@gmail.com>
2014-12-17 21:41:05 -0800

    bugfixing

--- app/components/articles/Article.jsx
+++ app/components/articles/Article.jsx
@@ -1,3 +1,4 @@
+var React = require('react');
 var ArticleItem = require('./ArticleItem');
 var Comment = require('./Comment');
 var ImmutableTreeNode = require('reapp-ui/helpers/ImmutableTreeNode');
