83ef2c26d63a009c4fe2da678d4940f25b5526a0
natew <natewienert@gmail.com>
2015-01-19 11:32:08 -0800

    Remove copied functions

--- app/components/articles/ArticleDrawer.jsx
+++ app/components/articles/ArticleDrawer.jsx
@@ -6,46 +6,6 @@ var Drawer = require('reapp-ui/components/Drawer');
 var View = require('reapp-ui/views/View');
 
 module.exports = Component({
-  getInitialState() {
-    return {
-      isRefreshing: false,
-      shownArticle: null
-    };
-  },
-
-  handleRefresh(e) {
-    if (this.state.isRefreshing)
-      return;
-
-    this.setState({ isRefreshing: true });
-
-    Actions.articlesHotLoad({ nocache: true }).then(() => {
-      this.setState({ isRefreshing: false });
-    });
-  },
-
-  handleLoadMore(e) {
-    e.preventDefault();
-    e.target.innerHTML = 'Loading...';
-
-    this.setState({ isRefreshing: true });
-
-    Actions.articlesHotLoadMore().then(() => {
-      this.setState({ isRefreshing: false });
-    });
-  },
-
-  handleArticlePress(id) {
-    // todo: make ui alerts and integrate
-    Actions.articleSave(id);
-  },
-
-  handleArticleClick(article) {
-    this.setState({
-      shownArticle: article
-    });
-  },
-
   render() {
     var { url } = this.props;
 
