fa7a537f9e8880551c96948dc5c009633fd1f3cb
natew <natewienert@gmail.com>
2015-01-21 13:45:35 -0800

    pass opts to webpackCallback

--- config/build.js
+++ config/build.js
@@ -2,4 +2,4 @@ var webpack = require('reapp-pack/webpack');
 var webpackCallback = require('reapp-pack/lib/callback');
 var webpackConfig = require('./build.config.js');
 
-webpack(webpackConfig, webpackCallback);
+webpack(webpackConfig, webpackCallback({ debug: true }));
