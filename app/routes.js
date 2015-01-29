dea846d6623f7bd8cccbd3c9275b2b5b959b7088
natew <natewienert@gmail.com>
2015-01-28 23:01:41 -0800

    remove handler keys

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 module.exports = ({ routes, route }) =>
   routes(require,
     route('articles', '/',
-      route('article', '/article/:id', { addHandlerKey: true }),
-      route('user', '/user/:id', { addHandlerKey: true })
+      route('article', '/article/:id'),
+      route('user', '/user/:id')
     )
   );
