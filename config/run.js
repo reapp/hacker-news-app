dac837be7cdfe1f5ab0b370a00f2eb5968b77b30
natew <natewienert@gmail.com>
2015-02-08 17:31:56 -0800

    new run config

--- config/run.js
+++ config/run.js
@@ -1,17 +1,22 @@
 var server = require('reapp-server');
 var webpackServer = require('reapp-pack/webpackServer');
+var makeLayout = require('reapp-pack/lib/makeLayout');
 var path = require('path');
 var webpackConfig = require('./run.config.js');
 var dir = path.join(__dirname, '..');
 
+var layout = makeLayout({
+  template: dir + '/assets/layout.html',
+  port: 3011,
+  scripts: Object.keys(webpackConfig.entry)
+})
+
 // express server
 server({
   dir: dir,
-  scripts: Object.keys(webpackConfig.entry),
-  layout: dir + '/assets/layout.html',
+  template: layout,
   debug: true,
-  port: 3010,
-  wport: 3011
+  port: 3010
 });
 
 // webpack-dev-server
